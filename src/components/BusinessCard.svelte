b6a964bd76fed8397f60974f6385b96142cd7456
Corentin <corentin.leguay@gmail.com>
2022-05-12 14:15:12 +0200

    Added base nav, added base of card

---
+++ src/components/BusinessCard.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let className = '';
+    export let title = 'Mr'; // Mr, Mme, Mlle, Dr, ...
+    export let firstname = '';
+    export let lastname = '';
+    export let adress = '';
+    export let cp = '';
+    export let city = '';
+    export let phoneNumber = '';
+    export let email = '';
+    export let job = '';
+
+    className = 'businessCard ' + className;
+</script>
+
+<div class={className}>
+    <p>{title}. {firstname} {lastname}</p>
+    <p>{job}</p>
+    <p>{adress}, {cp} {city}</p>
+    <p>{phoneNumber}</p>
+    <p>{email}</p>
+</div>
+
+<style>
+    .businessCard {
+        background-color: #B8B5FF;
+    }
+</style>
---
+++ src/views/CreateCardView.svelte
@@ -0,0 +1,7 @@
+<script>
+    import BusinessCard from '../components/BusinessCard.svelte';
+</script>
+
+<h1>Create card</h1>
+
+<BusinessCard title='Mr' firstname='Jean' lastname='Dupont' adress='1 rue de Machin' cp='75000' city='Paris' phoneNumber='0668898989' email='jean.dupont@email.com' job='Boulanger'/>
