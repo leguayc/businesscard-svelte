b5f69ff9e7ddcef558aecb853aa8610b46164c66
Paul Marechal <paulmarechal785@hotmail.fr>
2022-05-23 09:27:18 +0200

    slider small cards templates

--- src/views/CreateCardView.svelte
+++ src/views/CreateCardView.svelte
@@ -3,6 +3,7 @@
     import BusinessCard from '../components/BusinessCard.svelte';
     import FieldArea from '../components/FieldArea.svelte';
 	import { cardInfo } from '../stores/cardInfo.js';
+    import { gsap, Power1 } from "gsap";
 
     let forceValidation = false;
 
@@ -15,17 +16,63 @@
     }
 
     let templates = [
-		{ id: 0, className: 'template-box active' },
-		{ id: 1, className: 'template-box' },
-		{ id: 2, className: 'template-box' }
+		{ id: 0, className: 'template-box template-1 active' },
+		{ id: 1, className: 'template-box template-2' },
+		{ id: 2, className: 'template-box template-3' }
 	];
 
     let setTemplateActive = (id) => {
-        templates[$cardInfo.templateId].className = 'template-box';
-        templates[id].className = 'template-box active';
+        // templates[$cardInfo.templateId].className = 'template-box';
+        // templates[id].className = 'template-box  active';
         $cardInfo.templateId = id;
     }
+
+    const cards = ['.template-1', '.template-2', '.template-3']
+    const totalCards = 3;
+
+    let currentCard = 1;
+    let nextCard = currentCard + 1;
+    let lastCard =  nextCard + 1;
+
+    function filterActiveImages(template1, template2) {
+        const filteredResult = cards.filter((element) => element !== template1 && element !== template2)
+        return filteredResult;
+    }
+
+    function slideImage(currentCard,nextCard, lastCard ) {
+        let tl = gsap.timeline({defaults: {duration: 0.8, ease: Power1.easeInOut}});
+        tl.to(`.template-${currentCard}`, {rotation: -10, xPercent: -100})
+        .to(`.template-${currentCard}`, {rotation: 0, xPercent: 0})
+        .to(`.template-${nextCard}`, {zIndex: 2 }, "-=1.6")
+        .to(`.template-${currentCard}`, {zIndex: -1}, "-=1.6")
+        .to(`.template-${lastCard}`, {zIndex: 1}, "-=1.6")
+        .to(filterActiveImages(`.template-${nextCard}`,`.template-${currentCard}`), {zIndex:0}, "-=2.4")
+        tl.timeScale(2);
+        return tl;
+    }
+
+    function nextAnimation () {
+        slideImage(currentCard, nextCard, lastCard);
+        
+        currentCard = nextCard;
+        nextCard = currentCard - 1;
+
+        $cardInfo.templateId = currentCard;
+        
+        setTemplateActive(currentCard -1)
+        
+        if (currentCard === 1) {
+            nextCard = totalCards
+        }  
+            
+        if (nextCard === 1) {
+            lastCard = totalCards
+        }  else {
+            lastCard = nextCard - 1;
+        }
+    }
 </script>
+
     <article class="container">
     <div class="form-preview-card">
         <section class="card-preview">
@@ -92,11 +139,37 @@
             <h2>Choisis ton template</h2>
             <aside class="all-templates">
                 <!-- toggle class active -->
-                {#each templates as template}
-                    <div class={template.className} on:click={() => setTemplateActive(template.id)}></div>
-                {/each}
+                <div class="containerCard">
+                    {#each templates as template}
+                        <!-- <div class={template.className} on:click={() => setTemplateActive(template.id)}></div> -->
+                        <div class={template.className}></div>
+                    {/each}
+                </div>
+                
+                <button class="button button-next" on:click={() => nextAnimation()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <polyline points="9 6 15 12 9 18" />
+                    </svg>
+                </button>
             </aside>
             <button class="button-custom button-blue" on:click={save}>Suivant</button>
         </section>
     </div>
 </article>
+
+
+<style>
+.template-2 {
+  background-color: blue;
+}
+
+.template-3 {
+  background-color: black;
+
+}
+
+.template-1 {
+  background-color: red;
+}
+</style>
