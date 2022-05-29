b68ced74ea9314d9cf4e3c03372ab81e49b7db7f
AxelHuon <axelhuonpro@gmail.com>
2022-05-29 22:31:46 +0200

    update 2ème carte

--- src/components/bc-templates/BCTemplate2.svelte
+++ src/components/bc-templates/BCTemplate2.svelte
@@ -1,9 +1,25 @@
 <script>
     export let cardInfo = {};
+    console.log(cardInfo);
 </script>
 
-<p>{cardInfo.job}</p>
-<p>{cardInfo.title}. {cardInfo.firstname} {cardInfo.lastname}</p>
-<p>{cardInfo.phoneNumber}</p>
-<p>{cardInfo.email}</p>
-<p>{cardInfo.address}, {cardInfo.cp} {cardInfo.city}</p>
+
+<aside class="template-card-2">
+    <div class="logo-card">
+        <img src="../img/meta.png" alt="logo" />
+    </div>
+    <div class="infos-card">
+
+        <div class="header-card">
+            <p>{cardInfo.title}. {cardInfo.firstname} {cardInfo.lastname}</p>
+            <p>{cardInfo.job}</p>
+        </div>
+        <p class="email item-card">{cardInfo.email}</p>
+        <p class="phone item-card">{cardInfo.phoneNumber}</p>
+        <div class="address item-card">
+            <p>{cardInfo.address},</p>
+            <p >{cardInfo.cp}, {cardInfo.city}</p>
+        </div>
+
+    </div>
+</aside>
