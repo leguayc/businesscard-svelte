f27d12aabe02f1f1ee3f0c6ef9722f600ce361de
AxelHuon <axelhuonpro@gmail.com>
2022-05-30 20:10:55 +0200

    Last Card

--- src/components/bc-templates/BCTemplate2.svelte
+++ src/components/bc-templates/BCTemplate2.svelte
@@ -6,7 +6,7 @@
 
 <aside class="template-card-2">
     <div class="logo-card">
-        <img src="../img/meta.png" alt="logo" />
+        <img src="../img/airbnb.svg" alt="logo" />
     </div>
     <div class="infos-card">
 
@@ -14,11 +14,13 @@
             <p>{cardInfo.title}. {cardInfo.firstname} {cardInfo.lastname}</p>
             <p>{cardInfo.job}</p>
         </div>
-        <p class="email item-card">{cardInfo.email}</p>
-        <p class="phone item-card">{cardInfo.phoneNumber}</p>
-        <div class="address item-card">
-            <p>{cardInfo.address},</p>
-            <p >{cardInfo.cp}, {cardInfo.city}</p>
+        <div class="itmes-card-infos">
+            <p class="email item-card">{cardInfo.email}</p>
+            <p class="phone item-card">{cardInfo.phoneNumber}</p>
+            <div class="address item-card">
+                <p>{cardInfo.address},</p>
+                <p >{cardInfo.cp}, {cardInfo.city}</p>
+            </div>
         </div>
 
     </div>
